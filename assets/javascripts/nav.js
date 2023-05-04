19a076b3b0542e276031b809cd839fb5a19ed712
frederic <dewaele-frederic@outlook.fr>
2023-05-04 17:23:49 +0200

    refonte css fin

--- assets/javascripts/nav.js
+++ assets/javascripts/nav.js
@@ -1,14 +1,20 @@
 // dynamique navbar + option scroll
 let navSvg = document.getElementById("menu_toggle");
 let nav = document.getElementById("divNav");
+let container = document.querySelector(".container");
+let footer = document.querySelector(".footer");
 
 function navClick() {
   if (nav.classList == "navigation_none") {
     nav.classList.add("navigation");
     nav.classList.remove("navigation_none");
+    container.classList.add("filter-active");
+    footer.classList.add("filter-active");
   } else {
     nav.classList.add("navigation_none");
     nav.classList.remove("navigation");
+    container.classList.remove("filter-active");
+    footer.classList.remove("filter-active");
   }
 }
 
@@ -18,5 +24,7 @@ window.addEventListener("scroll", function () {
   if (window.scrollY > 0) {
     nav.classList.add("navigation_none");
     nav.classList.remove("navigation");
+    container.classList.remove("filter-active");
+    footer.classList.remove("filter-active");
   }
 });
